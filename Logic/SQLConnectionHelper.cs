cfa4157be3b077f575e39a81d5e76604d49b644d
Masoud Rahmani <mrem171@hotmail.com>
2020-01-03 08:49:53 +0330

    Version 1

---
+++ Logic/SQLConnectionHelper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLCalendarTable.Logic
+{
+    public class SQLConnectionHelper
+    {
+        private string _cnnSTR;
+
+        #region Property
+        public string Server { get; }
+        public string Database { get; set; }
+        public string TableName { get; set; }
+        public bool IntegratedSecurity { get; } 
+        #endregion
+
+        public SQLConnectionHelper(string server, bool integrated)
+        {
+            Server = server;
+            IntegratedSecurity = integrated;
+        }
+
+        public SqlConnection GetSqlConnection(ConnectionStringConfig instanceType)
+        {
+            var cnstr = new SqlConnectionStringBuilder
+            {
+                DataSource = Server,
+                IntegratedSecurity = IntegratedSecurity
+            };
+
+            SqlConnection sqlConnection;
+            switch (instanceType)
+            {
+                case ConnectionStringConfig.OnlyServer:
+                    {
+                        _cnnSTR = cnstr.ToString();
+                        sqlConnection = new SqlConnection(_cnnSTR);
+                        return sqlConnection;
+                    }
+                case ConnectionStringConfig.WithDatabase:
+                    {
+                        cnstr.InitialCatalog = Database;
+                        _cnnSTR = cnstr.ToString();
+                        sqlConnection = new SqlConnection(_cnnSTR);
+                        return sqlConnection;
+                    }
+                default:
+                    throw new Exception("No Instance");
+            }
+        }
+        public IEnumerable<object> GetDatabases()
+        {
+            using (var _sqlcnn = GetSqlConnection(ConnectionStringConfig.OnlyServer))
+            {
+                _sqlcnn.Open();
+                var d = _sqlcnn.GetSchema("Databases");
+                _sqlcnn.Close();
+
+                return (from DataRow rows in d.Rows select rows[0]).ToList();
+            }
+        }
+        public void CreateTable(string createCommand)
+        {
+            using (var sqlcnn = GetSqlConnection(ConnectionStringConfig.WithDatabase))
+            {
+                sqlcnn.Open();
+                using (var sqlCmd = new SqlCommand(createCommand, sqlcnn))
+                {
+                    sqlCmd.ExecuteNonQuery();
+                }
+                sqlcnn.Close();
+            }
+        }
+        public DataTable GetTableScheme()
+        {
+            var dt = new DataTable();
+            using (var sqlcnn = GetSqlConnection(ConnectionStringConfig.WithDatabase))
+            {
+                sqlcnn.Open();
+                using (var adaptor = new SqlDataAdapter(string.Concat("select * from ", TableName), sqlcnn))
+                {
+                    adaptor.FillSchema(dt, SchemaType.Source);
+                }
+            }
+            return dt;
+        }
+        public void BulkInsertData(DataTable dataTable)
+        {
+            using (var sqlcnn = GetSqlConnection(ConnectionStringConfig.WithDatabase))
+            {
+                sqlcnn.Open();
+                using (var bulk = new SqlBulkCopy(sqlcnn))
+                {
+                    bulk.DestinationTableName = TableName;
+
+                    bulk.WriteToServer(dataTable);
+
+                }
+                sqlcnn.Close();
+            }
+
+        }
+    }
+
+    public enum ConnectionStringConfig
+    {
+        OnlyServer,
+        WithDatabase
+    }
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using SQLCalendarTable.Logic;
+using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SQLCalendarTable
 {
@@ -20,9 +11,72 @@ namespace SQLCalendarTable
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _server;
+        private string _db;
+        private string _table;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        #region WPFEvent
+
+        private void ServerTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _server = ServerTextBox.Text;
+        }
+        private void DatabaseSelected(object sender, SelectionChangedEventArgs e)
+        {
+            _db = Databases.SelectedValue.ToString();
+        }
+        private void TableName_Changed(object sender, EventArgs e)
+        {
+            _table = TableNameTextBox.Text.ToString();
+        }
+
+        private void Fill_databases(object sender, RoutedEventArgs e)
+        {
+            var _helper = new SQLConnectionHelper(_server, (bool)Integrated.IsChecked);
+            Databases.ItemsSource = _helper.GetDatabases();
+        }
+
+        #endregion
+
+
+        private void StartToCreate(object sender, RoutedEventArgs e)
+        {
+            UpdateLayoutToInformStartofJob();
+
+            DateCreator bl = new DateCreator(_server, _db, _table, (bool)Integrated.IsChecked);
+
+            CalculateDates(bl);
+
+        }
+
+        private void UpdateLayoutToInformStartofJob()
+        {
+            Progressbar.IsIndeterminate = true;
+            MainGrid.IsEnabled = false;
+        }
+
+        private async void CalculateDates(DateCreator dc)
+        {
+            var start = (DateTime)StartDatePicker.SelectedDate;
+            var end = (DateTime)EndDatePicker.SelectedDate;
+            var result = Task.Factory.StartNew(() => { dc.Start(start, end); });
+            await result;
+            UpdateLayoutToInfromJobDone();
+
+        }
+        private void UpdateLayoutToInfromJobDone()
+        {
+            MainGrid.IsEnabled = true;
+            Progressbar.IsIndeterminate = false;
+            Progressbar.Maximum = 1;
+            Progressbar.Value = 1;
+            MessageBox.Show("Done.");
         }
     }
 }
